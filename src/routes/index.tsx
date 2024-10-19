8c304d79b76da9dc3b2c6be226a942ce5d702501
edi.brum <edineia.brum@maxiconsystems.com.br>
2024-10-19 11:47:54 -0300

    alterando logos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { CheckIn } from "@/pages/check-in";
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import { RoadMap } from "@/pages/road-map";
 
 export const router = createBrowserRouter([
   {
@@ -18,5 +19,9 @@ export const router = createBrowserRouter([
   {
     path: "/check-in",
     element: <CheckIn/>
+  },
+  {
+    path: "/road-map",
+    element: <RoadMap/>
   }
 ]);
